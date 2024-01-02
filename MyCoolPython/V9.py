68944c7fe0dca283c2884057cb0921f73ccbea97
Elvin <elvinlee528@gmail.com>
2024-01-02 13:14:51 -0500

    time.ssleeep

--- MyCoolPython/V9.py
+++ MyCoolPython/V9.py
@@ -1,4 +1,5 @@
 import msvcrt
+import time
 
 print("--------------------------------\n")
 print("the cool program.\n")
@@ -6,7 +7,7 @@ print("the cool program.\n")
 print("Press any key to continue...\n--------------------------------")
 msvcrt.getch()
 print("What's poppin'?")
-
+time.sleep(1.5)
 
 
 
